9b6ec58effeb4f22805aafc8ffb3a6ca96282b03
lucaaas <lucasoliveira2306@gmail.com>
2021-11-14 14:03:01 -0300

    Alterado estilo do app. #15

---
+++ lib/app/modules/entrada/new_entrada_page.dart
@@ -0,0 +1,60 @@
+import 'package:controlegastos/app/core/widgets/form_popup/form_popup.widget.dart';
+import 'package:controlegastos/app/core/widgets/selectize/selectize.widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'new_entrada_controller.dart';
+
+class NewEntradaPage extends StatefulWidget {
+  _NewEntradaPageState createState() => _NewEntradaPageState();
+}
+
+class _NewEntradaPageState extends State<NewEntradaPage> {
+  final TextEditingController _descricao = TextEditingController();
+  final TextEditingController _valor = TextEditingController();
+  final TextEditingController _data = TextEditingController();
+
+  final newEntradaController = new NewEntradaController();
+
+  _showDatePicker() async {
+    String data = await newEntradaController.openDatePicker(context);
+    _data.value = TextEditingValue(text: data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FormPopUpWidget(
+        fields: [
+          TextField(
+            decoration: InputDecoration(
+                labelText: 'Descrição', icon: Icon(Icons.textsms)),
+            controller: _descricao,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: 'Valor', icon: Icon(Icons.attach_money)),
+            controller: _valor,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: 'Data', icon: Icon(Icons.date_range)),
+            controller: _data,
+            readOnly: true,
+            onTap: _showDatePicker,
+          ),
+          Selectize(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              ElevatedButton(
+                child: Text('Salvar'),
+                onPressed: () => print('salvar'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
